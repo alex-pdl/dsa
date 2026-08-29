2bdb9dfbd8ec78e3db71496b50a203a64a1d22ae
Alexander Podlesny <apodlesny00@gmail.com>
2026-08-29 18:51:18 +0100

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.py
@@ -0,0 +1,51 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        num_and_sequence = {}
+        # Adding each num to dict
+        for num in nums:
+            num_and_sequence[num] = 0
+
+
+        seq_num = 1
+
+        for num in nums:
+            if num_and_sequence[num] != 0: 
+                continue
+
+            if num_and_sequence.get(num - 1) is None and \
+                num_and_sequence.get(num + 1) is None:
+                #print('im', num, 'and adjacent nums are empty')
+                continue
+
+            #print('for', num)
+            num_and_sequence[num] = seq_num
+
+            dist = 1
+            while num_and_sequence.get(num - dist) == 0:
+                #print('reassinging', num - dist, 'to', seq_num, '(its before)', num)
+                num_and_sequence[num - dist] = seq_num
+                dist += 1
+
+            dist = 1
+            while num_and_sequence.get(num + dist) == 0:
+                #print('reassinging', num + dist, 'to', seq_num, 'pos')
+                num_and_sequence[num + dist] = seq_num
+                dist += 1
+            
+            seq_num += 1
+        
+        mode = {}
+        for val in num_and_sequence.values():
+            if val == 0: continue
+
+            if mode.get(val) is None:
+                mode[val] = 0
+            
+            mode[val] += 1
+
+        if len(mode) == 0:
+            return 1
+        
+        return max(mode.values())
