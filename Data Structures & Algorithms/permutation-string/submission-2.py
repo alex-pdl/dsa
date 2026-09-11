dd6fd8814394c3fa04eeab00974ac18bc07f9f71
Alexander Podlesny <apodlesny00@gmail.com>
2026-09-11 17:17:03 +0100

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # BRUTE FORCE
+        n = len(s2)
+        s1_char_count = {}
+        for i in s1:
+            if s1_char_count.get(i) is None:
+                s1_char_count[i] = 0
+            s1_char_count[i] += 1
+
+        for l in range(n):
+            substr_char_count = {}
+            
+            for r in range(l, n):
+                if substr_char_count.get(s2[r]) is None:    
+                    substr_char_count[s2[r]] = 0
+                
+                substr_char_count[s2[r]] += 1
+            
+                if substr_char_count == s1_char_count:
+                    return True
+        
+        return False
